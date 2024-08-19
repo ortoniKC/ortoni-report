eb677e559e40adaf1a254a1ea958e93f18c4ece2
ortoniKC <koushik350@gmail.com>
2024-08-19 23:47:44 +0530

    🚀 RELEASE: V2.0.2

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -24,21 +24,21 @@ export function msToTime(duration: number): string {
     }
 
     return result;
-};
+}
 
 export function normalizeFilePath(filePath: string): string {
     // Normalize the path to handle different separators
     const normalizedPath = path.normalize(filePath);
     // Get the base name of the file (removes any leading directories)
     return path.basename(normalizedPath);
-};
+}
 export function formatDate(date: Date) {
     const day = String(date.getDate()).padStart(2, '0');
     const month = date.toLocaleString('default', { month: 'short' });
     const year = date.getFullYear();
     const time = date.toLocaleTimeString();
     return `${day}-${month}-${year} ${time}`;
-};
+}
 
 export function safeStringify(obj: any, indent = 2) {
     const cache = new Set();
@@ -53,11 +53,11 @@ export function safeStringify(obj: any, indent = 2) {
     }, indent);
     cache.clear();
     return json;
-};
+}
 export function ensureHtmlExtension(filename: string) {
     const ext = path.extname(filename);
     if (ext && ext.toLowerCase() === '.html') {
         return filename;
     }
     return `${filename}.html`;
-};
+}
