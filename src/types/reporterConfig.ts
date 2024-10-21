a6882f2722822c4c756a422081852f0728ce8a05
ortoniKC <koushik350@gmail.com>
2024-10-21 14:30:12 +0530

    📖 DOC: Improved

--- src/types/reporterConfig.ts
+++ src/types/reporterConfig.ts
@@ -5,37 +5,35 @@ export interface OrtoniReportConfig {
     /**
      * Open the report in local host (Trace viewer is accessible only in localhost)
      * @example "always"| "never"| "on-failure";
+     * @default "always"
      */
-
-    open?:"always"| "never"| "on-failure";
+    open?: "always" | "never" | "on-failure";
     /**
      * The title of the HTML report.
      * @example "Ortoni Playwright Test Report"
      */
-    title?: string
+    title?: string;
     /**
     * Add project on the list of the tests? (Filtering projects still works if hidden)
     * @example true to display, false to hide.
+    * @default false
     */
-    showProject?: boolean
+    showProject?: boolean;
     /**
      * The name of the project.
      * @example "Ortoni Project"
      */
     projectName?: string;
-
     /**
      * The name of the author.
-     * @example "John Doe"
+     * @example "Koushik Chatterjee"
      */
     authorName?: string;
-
     /**
      * The type of tests being run.
      * @example "Regression"
      */
     testType?: string;
-
     /**
      * The preferred theme for the report.
      * Can be either "light" or "dark".
@@ -43,29 +41,28 @@ export interface OrtoniReportConfig {
      * @example "dark"
      */
     preferredTheme?: 'light' | 'dark';
-
     /**
      * If true, images will be encoded in base64.
      * @default false
      * @example true
      */
     base64Image?: boolean;
-
     /**
      * The local relative of the logo image.
      * Recommended to keep within the ${folderPath} folder
-     * @example "./assets/logo.png"
-     * @example "/absolute/path/to/logo.png"
+     * @example "folderPath/logo.png"
      */
     logo?: string;
     /**
     * The filename to the html report.
     * @example "index.html"
+    * @default "ortoni-report.html"
     */
     filename?: string;
     /**
     * The folder name.
-    * @example "ortoni-report"
+    * @example "report"
+    * @default "ortoni-report"
     */
     folderPath?: string;
     /**
@@ -73,7 +70,6 @@ export interface OrtoniReportConfig {
     * @example 3600
     */
     port?: number;
-
 }
 
 /**
