c15c2e4779e549bf5dba5638a709b13ed480e06e
Koushik Chatterjee <koushik350@gmail.com>
2025-08-22 21:54:23 +0530

    👌 IMPROVE: removed logs

--- src/helpers/fileManager.ts
+++ src/helpers/fileManager.ts
@@ -17,7 +17,6 @@ export class FileManager {
   writeReportFile(filename: string, data: unknown): string {
     const templatePath = path.resolve(__dirname, "index.html");
     let html = fs.readFileSync(templatePath, "utf-8");
-    console.log(html);
     const reportJSON = JSON.stringify({
       data,
     });
